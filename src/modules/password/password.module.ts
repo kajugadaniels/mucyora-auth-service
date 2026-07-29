436fb6e41e7b4bf6bc5f37c40828d7f33d35c0b9
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-29 22:02:03 +0200

    feat(auth): register password lifecycle services

--- src/modules/password/password.module.ts
+++ src/modules/password/password.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { IntegrationsModule } from '../../integrations/integrations.module';
+import { AuthModule } from '../auth/auth.module';
+import { PasswordController } from './password.controller';
+import { PasswordRateLimiter } from './password-rate-limiter.service';
+import { PasswordService } from './password.service';
 
-@Module({})
+@Module({
+  imports: [AuthModule, IntegrationsModule],
+  controllers: [PasswordController],
+  providers: [PasswordRateLimiter, PasswordService],
+})
 export class PasswordModule {}
