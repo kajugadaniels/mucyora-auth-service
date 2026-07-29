7ab43c8f671ea9cb856aff0a602a6fed818f064e
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-29 20:48:05 +0200

    feat(identity): configure registration challenge module

--- src/modules/registration/registration.module.ts
+++ src/modules/registration/registration.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { IntegrationsModule } from '../../integrations/integrations.module';
+import { SecurityEventsModule } from '../security-events/security-events.module';
+import { CitizenLookupRateLimiter } from './citizen-lookup-rate-limiter.service';
+import { CitizenLookupService } from './citizen-lookup.service';
+import { RegistrationChallengeLifecycleService } from './registration-challenge-lifecycle.service';
+import { RegistrationChallengeTokenService } from './registration-challenge-token.service';
+import { RegistrationController } from './registration.controller';
 
-@Module({})
+@Module({
+  imports: [IntegrationsModule, SecurityEventsModule],
+  controllers: [RegistrationController],
+  providers: [
+    CitizenLookupRateLimiter,
+    CitizenLookupService,
+    RegistrationChallengeLifecycleService,
+    RegistrationChallengeTokenService,
+  ],
+  exports: [
+    RegistrationChallengeLifecycleService,
+    RegistrationChallengeTokenService,
+  ],
+})
 export class RegistrationModule {}
