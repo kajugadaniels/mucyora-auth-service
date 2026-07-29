8ef4ccbe2113a4fac0c44c8e86ba2f408cad8150
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-29 21:08:12 +0200

    feat(auth): configure email verification module

--- src/modules/email-verification/email-verification.module.ts
+++ src/modules/email-verification/email-verification.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MailModule } from '../../integrations/mail/mail.module';
+import { RegistrationModule } from '../registration/registration.module';
+import { EmailVerificationController } from './email-verification.controller';
+import { EmailVerificationService } from './email-verification.service';
 
-@Module({})
+@Module({
+  imports: [RegistrationModule, MailModule],
+  controllers: [EmailVerificationController],
+  providers: [EmailVerificationService],
+})
 export class EmailVerificationModule {}
