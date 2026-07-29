7451eae8a21a0c2e53fb8477cbb5f6f0ab71b6d0
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-29 19:16:40 +0200

    feat(auth): export common database infrastructure

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule],
+  exports: [DatabaseModule],
+})
 export class CommonModule {}
