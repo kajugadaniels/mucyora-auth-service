23067826b6e797bc2813baa2520c60edd2710eea
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-29 21:07:58 +0200

    feat(auth): export mail integration

--- src/integrations/integrations.module.ts
+++ src/integrations/integrations.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CitizenApiModule } from './citizen-api/citizen-api.module';
 import { RedisModule } from './redis/redis.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [RedisModule, CitizenApiModule],
-  exports: [RedisModule, CitizenApiModule],
+  imports: [RedisModule, CitizenApiModule, MailModule],
+  exports: [RedisModule, CitizenApiModule, MailModule],
 })
 export class IntegrationsModule {}
