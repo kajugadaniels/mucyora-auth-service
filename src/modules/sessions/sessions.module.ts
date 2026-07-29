10f40a3d9c39a23867ebe4556474ee52003c56e3
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-29 21:45:17 +0200

    feat(sessions): register session management services

--- src/modules/sessions/sessions.module.ts
+++ src/modules/sessions/sessions.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { SessionManagementService } from './session-management.service';
+import { SessionsController } from './sessions.controller';
 
-@Module({})
+@Module({
+  imports: [AuthModule],
+  controllers: [SessionsController],
+  providers: [SessionManagementService],
+})
 export class SessionsModule {}
