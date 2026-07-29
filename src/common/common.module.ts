ea673e0b4861cde0a5b27e691146a846b36bac21
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-29 19:58:41 +0200

    feat(auth): expose security infrastructure

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { SecurityPrimitivesModule } from './security/security-primitives.module';
 
 @Module({
-  imports: [DatabaseModule],
-  exports: [DatabaseModule],
+  imports: [DatabaseModule, SecurityPrimitivesModule],
+  exports: [DatabaseModule, SecurityPrimitivesModule],
 })
 export class CommonModule {}
