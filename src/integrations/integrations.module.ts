1f48840f1b28fbecd33d3455b2b26211d25a9e68
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-29 22:52:00 +0200

    feat(identity): register verification integrations

--- src/integrations/integrations.module.ts
+++ src/integrations/integrations.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { CitizenApiModule } from './citizen-api/citizen-api.module';
 import { RedisModule } from './redis/redis.module';
 import { MailModule } from './mail/mail.module';
+import { EngineModule } from './engine/engine.module';
+import { VerificationStorageModule } from './verification-storage/verification-storage.module';
 
 @Module({
-  imports: [RedisModule, CitizenApiModule, MailModule],
-  exports: [RedisModule, CitizenApiModule, MailModule],
+  imports: [
+    RedisModule,
+    CitizenApiModule,
+    MailModule,
+    EngineModule,
+    VerificationStorageModule,
+  ],
+  exports: [
+    RedisModule,
+    CitizenApiModule,
+    MailModule,
+    EngineModule,
+    VerificationStorageModule,
+  ],
 })
 export class IntegrationsModule {}
